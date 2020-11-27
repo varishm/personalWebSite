05d70a5074120094ffdfa30d767cb134d1f48534
Varish Mulwad <varishmulwad@gmail.com>
2020-11-26 19:46:42 -0500

    updated json file path

--- assets/js/main.js
+++ assets/js/main.js
@@ -7,7 +7,7 @@ function main(){
 function renderAwards() {
     loadJSON(function(response) {
         // Parsing JSON string into object
-          var actual_JSON = JSON.parse(response);git 
+          var actual_JSON = JSON.parse(response);
           console.log(actual_JSON)
        });
       
