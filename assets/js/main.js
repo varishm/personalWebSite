cf6de578d20de48e72eb1bace1ee4beac747a4d9
Varish Mulwad <varishmulwad@gmail.com>
2020-11-27 18:41:51 -0500

    render academic service section via JSON

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,41 @@
 $(document).ready(main);
 
 function main(){
+    renderAcademicService();
     renderAwards();
+}
+
+
+function renderAcademicService(){
+    loadJSON('assets/info/academic-service.json', function(response) {
+        // Parsing JSON string into object
+        var service = JSON.parse(response);
+        var misc = service["misc"];
+        var pc = service["program_committee"]
+        var reviewer = service["reviewer"]
+        
+        updateServiceHTML(misc, "service")
+        updateServiceHTML(pc, "pc")
+        updateServiceHTML(reviewer, "reviewer")
+       });
+}
+
+function updateServiceHTML(arr, elemID){
+
+    for (idx = 0; idx < arr.length; idx++){
+        var service = arr[idx];
+        var line = service["venue"] + ", " + service["year"]
+        if ('link' in service){
+            link = '<a href="' + service['link'] + '" target="_blank">link</a>'
+            line = line + " " + link
+        }
+        if (elemID == 'service'){
+            $("#" + elemID).prepend("<li>" + line + "</li>")
+        }
+        else{
+            $("#" + elemID).append("<li>" + line + "</li>")
+        }
+    }
 }
 
 function renderAwards() {
