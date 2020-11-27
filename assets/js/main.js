6e37aec06935212dab6055d1059b444b36a7d7f8
Varish Mulwad <varishmulwad@gmail.com>
2020-11-26 19:40:04 -0500

    fixed json file path

--- assets/js/main.js
+++ assets/js/main.js
@@ -18,7 +18,7 @@ function loadJSON(callback) {
 
     var xobj = new XMLHttpRequest();
         xobj.overrideMimeType("application/json");
-    xobj.open('GET', '../info/awards.json', true); // Replace 'appDataServices' with the path to your file
+    xobj.open('GET', 'assets/info/awards.json', true); // Replace 'appDataServices' with the path to your file
     xobj.onreadystatechange = function () {
           if (xobj.readyState == 4 && xobj.status == "200") {
             // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
